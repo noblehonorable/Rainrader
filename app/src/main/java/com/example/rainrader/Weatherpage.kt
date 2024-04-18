4886f58a072f27fa5a7478ec720839ab92cead6c
i499804@fhict.nl <i499804@fhict.nl>
2024-04-18 19:27:13 +0200

    Realtime date has been added

--- app/src/main/java/com/example/rainrader/Weatherpage.kt
+++ app/src/main/java/com/example/rainrader/Weatherpage.kt
@@ -1,8 +1,11 @@
 package com.example.rainrader
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import java.text.DateFormat
+import java.util.Calendar
 
 
 class Weatherpage : AppCompatActivity() {
@@ -10,6 +13,11 @@ class Weatherpage : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_weatherpage)
+
+        val calendar = Calendar.getInstance().time
+        val dateFormat = DateFormat.getDateInstance(DateFormat.FULL).format(calendar)
+        val dateTextView = findViewById<TextView>(R.id.current_date)
+        dateTextView.text = dateFormat
 
     }
 }
